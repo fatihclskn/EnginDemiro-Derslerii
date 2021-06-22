36a0fec24e5f370dabec1d0941a392053ef7b63f
fatih çalışkan <fatihclskn132@gmail.com>
2021-06-23 02:45:58 +0300

    23 HAZİRAN 2021

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+
+        }
+
+
+
+
+        public void Add(T item)
+        {
+
+
+        }
+
+
+
+    }
+}
